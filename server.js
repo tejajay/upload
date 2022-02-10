0ab1875c7521779a93cc4ad2089e18084831e3c6
tejajay <83235989+tejajay@users.noreply.github.com>
2022-02-10 10:27:45 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const initRoutes = require("./routes");
+var corsOptions = {
+  origin: "http://localhost:5000"
+};
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+let port = 5000;
+app.listen(port, () => {
+  console.log(`Running at localhost:${port}`);
+});
